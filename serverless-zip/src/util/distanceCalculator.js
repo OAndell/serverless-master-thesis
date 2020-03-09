97d1842d5aacc0fbac256b804267378825eae455
OAndell <Oscar@Andell.eu>
2020-03-09 09:12:38 +0100

    serverless-zip files

---
+++ serverless-zip/src/util/distanceCalculator.js
@@ -0,0 +1,19 @@
+
+module.exports.calculateDistance = function(posistion1, posistion2){
+    let lat1 = posistion1.lat;
+    let lon1 =  posistion1.long;
+    let lat2 = posistion2.lat;
+    let lon2 =  posistion2.long;
+
+    var R = 6371; // km (change this constant to get miles)
+	var dLat = (lat2-lat1) * Math.PI / 180;
+	var dLon = (lon2-lon1) * Math.PI / 180;
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+		Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
+		Math.sin(dLon/2) * Math.sin(dLon/2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+	var d = R * c;
+	if (d>1) return Math.round(d)+"km";
+	else if (d<=1) return Math.round(d*1000)+"m";
+	return d;
+};
